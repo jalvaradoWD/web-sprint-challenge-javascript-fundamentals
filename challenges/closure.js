9e0b5d791479fe554ad308491217cab3584ffd84
jalvaradoWD <josealvarado1337@gmail.com>
2020-07-17 18:32:30 -0500

    finished with closure.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -1,7 +1,6 @@
-// ==== Closures ==== 
+// ==== Closures ====
 
 /* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
-
 
 const external = "I'm outside the function";
 
@@ -11,14 +10,24 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
-  };
+  }
   nestedFunction();
 }
 myFunction();
 
-// Explanation: 
-
+// Explanation: Since the nestedFunction is in myFunction, nestedFunction can access all of the variables in the global scoped and all of the variables within myFunction.
+// If nestedFunction were to be outside of the myFunction scope, then it can no longer access the variables within myFunction.Thus give the nestedFunction a reference error since it there is no instance of the "internal" variable in the current scope in nestedFunction.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const summation = (num) => {
+  let counter = 0;
+  for (let i = 0; i < num; i++) {
+    counter += i + 1;
+  }
+  return counter;
+};
+
+console.log(summation(4));
